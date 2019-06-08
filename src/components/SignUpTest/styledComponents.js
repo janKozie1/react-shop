f6ea6e0d5dc4dc706987f97f5ae73b945c02445b
janKozie1 <jankkoziel@gmail.com>
2019-06-08 22:46:38 +0200

    Redeisgned sign up page

---
+++ src/components/SignUpTest/styledComponents.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+import bg from '../../images/1.jpg'
+import {colors} from '../cssVariables'
+import {NavLink} from 'react-router-dom'
+export let SignUp = styled.section`
+    grid-column:1/4;
+    background:white;
+    width:100%;
+    height:100%;
+    max-height:100%;
+    display:flex;
+    
+`
+export let Leaflet = styled.div`
+    flex:1;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)), url(${bg});
+    background-position:center top;
+`
+export let FormSection = styled.section`
+    
+   padding:0 150px;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+    justify-content:space-around;
+    >h2{
+        margin-top:60px;
+        >span{
+            color:${colors.primaryColor};
+        }
+    }
+`
+
+export let FormContainer = styled.form`
+    border:1px solid rgba(0,0,0,0.3);
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    flex-direction:column;
+    width:600px;
+    padding:30px 60px;
+    border-radius:10px;
+    margin-bottom:60px;
+`
+
+export let Label = styled.label`
+    width:100%;
+    display:flex;
+    flex-direction:column;
+    align-items:space-between;
+    justify-content:space-between;
+    padding:20px 0px;
+`
+
+export let Input = styled.input`
+    flex:1;
+    margin-top:15px;
+    padding:10px 20px;
+    border:none;
+    border-bottom:1px solid ${colors.dividerColor};
+    
+
+`
+
+export let ActionContainer = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    padding: 20px 0;
+    width:100%;
+`
+export let Link = styled(NavLink)`
+    font-size:0.8rem;
+    color:black;
+    text-decoration:none;
+`
+export let SubmitButton = styled.button`
+    background:${colors.primaryColor};
+    padding:10px 30px;
+    border:none;
+    border-radius:5px;
+    color:white;
+`
